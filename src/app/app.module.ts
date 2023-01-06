81548bf11db0e9c6441e67d672739330da54f9c6
Jyoti Chawla <jyotichawla459@gmail.com>
2023-01-06 16:55:31 +0530

    angular_template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordPatternDirective } from './directives/password-pattern.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordPatternDirective
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
